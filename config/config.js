ebead04de7142193a77a46a9bfeb10ae7f976ab3
unknown <juanfilapablo@hotmail.com>
2023-02-10 21:18:03 -0300

    first commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "juan",
+    password: "2771az",
+    database: "curso_sequelize",
+    define: {
+      timestamps: true,
+    },
+  };
